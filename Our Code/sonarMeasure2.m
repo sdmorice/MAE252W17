044462ba4853e73078f7295d2df65686bb4eb68c
rtmuradian <rtmuradian@ucdavis.edu>
2017-02-26 15:55:50 -0800

    fix sonar calcs

--- Our Code/sonarMeasure2.m
+++ Our Code/sonarMeasure2.m
@@ -4,14 +4,13 @@ function ds =sonarMeasure2(grid_map,rpos,ns, range)
 %       traveling
 %       -(1)'s make up obstacles
 %       -each point is 1cm (grid size)
-%       -start: (-1)
-%       -end: (-2)
-%   rpos:[xpos, ypos, angle]
-%       -where angle is the angle that the robot is x axis of the course, in rad
+%   rpos = [xpos, ypos, angle]
+%       -angle: measured from x-axis from centerpoint of front of robot
+%       (aka y-axis of robot)
 %   ns: number of sensors
 %   range: max distance the sonar can sense, cm
-
-%   target = 1mx1m
+%
+%   The 
 
 %setup location of the robot 
 rx = rpos(1);                 
@@ -34,17 +33,14 @@ minRow = rx - range;
 maxColumn = ry + range;
 minColumn = ry - range; 
 
-obstacleArray =  zeros(m+n, 4);       %need to change size
-k = 1;
 %target = zeros(m+n, 2);               %need to change size 
 minDistArray = 20000*ones(1, ns);
 
 %iterate through array to find the obstacles, distance and angleof each point 
 for i = max([minRow 1]): min([maxRow m])
     for j = max([minColumn 1]):min([maxColumn n])
-        %need to be 1 instead of 0 because of matlab indexing
         if grid_map(i, j) == 1
-            
+            q = q+1;
             distPt = sqrt((i - rx)^2 + (j-ry)^2);
             worldAng = atan2(j-ry,i-rx);               %find angle of point - from 0
             
@@ -58,31 +54,28 @@ for i = max([minRow 1]): min([maxRow m])
             bodyAng = worldAng - rAngle + pi/2;
             
             if bodyAng>= 2*pi
-                bodyAng = bodyAng/(2*pi);
+                bodyAng = bodyAng-(2*pi);
             end
             
             sonarPt = ceil(bodyAng/sonarBeamAngle);     %returns index of sonar 
-            
-            %also need to check that angle is within angle ranges for the
-            %sensor
-            if sonarPt < 0 || sonarPt > ns
-                break
-            elseif sonarPt == 0
+            if sonarPt == 0 
                 sonarPt = 1;
             end
             
-            %save value if within range and less than the smalledst value
-            %sensed yet
-            if distPt < minDistArray(sonarPt) 
-                if distPt > range
-                    minDistArray(sonarPt) = 0;  %check if inside range
-                else
-                    minDistArray(sonarPt) = distPt;
-                end
-            end 
+            %also need to check that angle is within angle ranges for the
+            %sensor
+            if sonarPt > 0 && sonarPt <= ns
             
-            obstacleArray(k, :) = [i, j, distPt, sonarPt];
-            k=k+1;
+                %save value if within range and less than the smalledst value
+                %sensed yet
+                if distPt < minDistArray(sonarPt) 
+                    if distPt > range
+                        minDistArray(sonarPt) = 0;  %check if inside range
+                    else
+                        minDistArray(sonarPt) = distPt;
+                    end
+                end  
+            end
         end
     end 
 end
