512e288dfc459362dd336c6018fab72310783223
rtmuradian <rtmuradian@ucdavis.edu>
2017-03-09 10:01:58 -0800

    goal finding

---
+++ Our Code/goal_finding.m
@@ -0,0 +1,17 @@
+function [dist_to_goal, goal_found] = goal_finding(rpos, robot_goal)
+
+dist_to_goal(1) = sqrt((rpos(2) - robot_goal(2))^2 + (rpos(1) - robot_goal(1))^2);
+dist_to_goal(2) = atan2((rpos(2) - robot_goal(2)),(rpos(1) - robot_goal(1)));
+
+if dist_to_goal(2) < 0
+    dist_to_goal(2) = dist_to_goal(2) + 2*pi;
+end
+
+%check if essentially reached goal
+if dist_to_goal(1) < 7
+    goal_found = 1; 
+else 
+    goal_found = 0;
+end
+
+end
