4450a1f86457a633ea002069bede6e666d51fd05
Daniel D Genova <ddgenova@ucdavis.edu>
2017-03-12 17:35:45 -0700

    stuff

--- Our Code/goal_finding.m
+++ Our Code/goal_finding.m
@@ -27,7 +27,7 @@ if  2*pi> rpos(3) && rpos(3)>= pi
         to_goal(2) = worldGoalAng + 2*pi - rpos(3); 
     end
 elseif  pi> rpos(3) && rpos(3) >= 0
-   if rpos(3)+ pi > worldGoalAng && rpos(3) > worldGoalAng
+   if rpos(3)+ pi > worldGoalAng && worldGoalAng > 0
        to_goal(2) = worldGoalAng - rpos(3);
    else
        to_goal(2) = worldGoalAng - 2*pi - rpos(3);
