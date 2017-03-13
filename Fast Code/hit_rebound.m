014f8d7b74ee7ff3c2875bf416e671e0c255f2f4
sdmorice <sdmorice@ucdavis.edu>
2017-03-13 14:43:57 -0700

    Add files via upload

---
+++ Fast Code/hit_rebound.m
@@ -0,0 +1,90 @@
+function alpha_r = rebound_angle_goal(distSonar,ns, rpos, robot_goal, range)
+
+
+
+%calculate rebound angle
+
+
+%initialize numerator and denomenator values for the rebound angle
+%calculation
+num_sum = 0;
+den_sum = 0;
+max_sensor_index = ns/2;
+min_sensor_index = -ns/2;
+sonarBeamAngle = pi/ns;
+
+leftFlag = 0;
+rightFlag =0;
+leftFlag_goal = 0;
+rightFlag_goal =0;
+num_sum_1 = 0;
+num_sum_2 = 0;
+den_sum_1 = 0;
+den_sum_2 = 0;
+
+%want to find rebound angles for both sides of the vision system
+for i = 1:floor(ns/2)
+    sensor_index = (i-1)+min_sensor_index;
+   
+    num_add = sensor_index*sonarBeamAngle*distSonar(1,i);
+    num_sum_1 = num_sum_1+num_add;
+    
+    den_add = distSonar(1,i);
+    den_sum_1 = den_sum_1+den_add;
+    
+    if distSonar(1, i) == 0 || distSonar(1, i)> range/2
+        leftFlag=leftFlag+1; 
+    end
+end
+
+for i = floor(ns/2):ns
+    sensor_index = (i)+min_sensor_index;
+   
+    num_add = sensor_index*sonarBeamAngle*distSonar(1,i);
+    num_sum_2 = num_sum_2+num_add;
+    
+    den_add = distSonar(1,i);
+    den_sum_2 = den_sum_2+den_add;
+    
+    if distSonar(1, i) == 0 || distSonar(1, i)> range/2
+        rightFlag=rightFlag+1; 
+    end
+end
+
+
+
+%% goal weighting
+k = 100;
+[to_goal, goal_found] = goal_finding(rpos, robot_goal);
+sensor_index_goal = to_goal(2)/sonarBeamAngle;
+
+%calc related sensor index for goals taking into account situation where
+%goal may not be with in range of sensors
+if sensor_index_goal < ns/2 || (sensor_index_goal<ns*2 && sensor_index_goal>ns*3/2)
+    sensor_index_goal = (i-1)+min_sensor_index;
+    leftFlag_goal = 1;
+else
+    sensor_index_goal = (i)+min_sensor_index;
+    rightFlag_goal = 1;
+end
+
+if abs(leftFlag-rightFlag) <= 2 && (leftFlag~=0 || rightFlag~=0)
+    disp('equal weightings') 
+    %want the angle of rebound to be in the direction of the goal 
+    if leftFlag_goal
+        num_sum = num_sum_1;
+        den_sum = den_sum_1;
+    else 
+        num_sum = num_sum_2; 
+        den_sum = den_sum_2; 
+    end
+    
+else 
+    num_sum = num_sum_1 + num_sum_2;
+    den_sum = den_sum_1 + den_sum_2; 
+end
+
+%% Calculattng the rebound angle
+%if no objects, keep on going straight    
+
+alpha_r = num_sum/den_sum;
---
+++ Fast Code/main_simulation_test.m
@@ -0,0 +1,99 @@
+%% Robot Test Script
+
+clear
+clc
+close all
+
+hold all;
+
+global hit_counter
+hit_counter = 0;
+%define robot information 
+
+
+range = 500 + 30;
+ns = 8;
+
+iter = 0;
+
+time = 0;
+
+%initial positon
+rpos = [720, 60, pi/2];
+%rpos = [200, 1000, pi/2];
+goalReached = 0;
+
+%goal locations
+goal_1 = [60, 800];
+goal_2 = [600, 1380]; 
+robot_goal = goal_1;
+k = 0;
+
+%start timer of the code
+tic 
+
+%map definition 
+grid_map= color_gry('prescribed_course.png');
+
+%plot robot
+% draw_bot(rpos, grid_map);
+% drawnow;
+
+
+%% Robot Moving Loop
+
+while ~goalReached && time < 240
+    iter = iter + 1;
+    [sonarDist, goalSighted, goalReached] = sonarMeasure2(grid_map, rpos, ns, range);
+    
+    sonarDist;
+
+    
+    [dist_to_goal, goal_found] = goal_finding(rpos, robot_goal);
+    
+    if goal_found
+        robot_goal = goal_2; 
+        k = k+1; 
+        disp(k);
+        if k >=2
+            goalReached = 1; 
+        end
+    end
+    
+    alpha_r = rebound_angle(sonarDist, ns, rpos, robot_goal);      
+    [rpos, hit, steps] = Rotate(rpos, alpha_r, grid_map);
+    time = time + 0.01*steps;
+    rpos =  drive(rpos, grid_map, hit, robot_goal);
+    time = time + 0.05;
+    
+    x_save(iter) = rpos(1);
+    y_save(iter) = rpos(2);
+    
+%     plot(y_save, x_save);
+%     pause(0.01)
+
+    
+end
+
+draw_bot(rpos, grid_map);
+plot(y_save, x_save);
+
+
+%stop timer 
+timeRun = toc; 
+fprintf('The simulation ran for %f seconds.\n',timeRun);
+
+% %save plot as figure 
+% saveas(figure1,'plot.jpg'); 
+
+%%plot the path
+dist = 0;
+for j = 1:iter-1
+
+    distj = sqrt((x_save(j+1) - x_save(j))^2 + (y_save(j+1) - y_save(j))^2);
+    dist = distj + dist;
+end
+
+fprintf('The robot traveled %f cm.\n', dist)
+
+disp(hit_counter);
